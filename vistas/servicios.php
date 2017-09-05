3256b0fd484de46482a4cef0286dbe0d4ae86c88
esteban1234 <tva_libra@hotmail.com>
2017-09-05 18:15:51 -0500

    servicios listo

--- vistas/servicios.php
+++ vistas/servicios.php
@@ -2,211 +2,101 @@
 <?php include 'encabezado2.php' ?>
 <body>
 <header>
-  <figure class="logo">
-    <img src="../img/nokin.png" class="img-responsive" alt="">
+  <figure class="logo pull-right">
+    <img src="../img/inpet.png" alt="INPET" class="img-responsive">
   </figure>
-  <!-- <div class="tch">
-    <span class="fa-stack fa-lg">
-      <i class="fa fa-circle fa-stack-2x"></i>
-      <i class="fa fa-phone fa-stack-1x fa-inverse"></i>
-    </span>
-            <span style="color: #65BFB4; font-size: 17px;">PENDIENTE</span><br>
-    <span class="fa-stack fa-lg">
-      <i class="fa fa-circle fa-stack-2x"></i>
-      <i class="fa fa-clock-o fa-stack-1x fa-inverse"></i>
-    </span>
-            <span style="color: #65BFB4; font-size: 17px;">Lunes a Viernes de 9am a 6pm</span><br>
-    <span class="fa-stack fa-lg">
-      <i class="fa fa-circle fa-stack-2x"></i>
-      <i class="fa fa-envelope-o fa-stack-1x fa-inverse"></i>
-    </span>
-            <span style="color: #65BFB4; font-size: 17px;">info@nokinservicios.com</span><br>
-  </div> -->
-
-  <div class="menu">
+  <nav class="pull-left">
     <ul>
-      <a class="marca" href="../index.php">INICIO</a>
-      <a class="marca" href="nosotros.php">NOSOTROS</a>
-      <a class="marca select" href="servicios.php">SERVICIOS</a>
-      <a class="marca" href="contacto.php">CONTACTO</a>
+      <a class="selector" href="../index.php"><i class="fa fa-bars" aria-hidden="true"></i> INICIO</a>
+      <a class="selector" href="nosotros.php"><i class="fa fa-users" aria-hidden="true"></i> NOSOTROS</a>
+      <a class="selector" id="select" href="servicios.php"><i class="fa fa-folder" aria-hidden="true"></i> SERVICIOS</a>
+      <a class="selector" href="contacto.php"><i class="fa fa-tty" aria-hidden="true"></i> CONTACTO</a>
     </ul>
-    <div class="menu-openclose">
-      <i class="fa fa-bars fa-3x open" aria-hidden="true"></i>
-      <i class="fa fa-times fa-3x close oculto" style="color:green" aria-hidden="true"></i>
-    </div>
-  </div>
+  </nav>
 </header>
 
-<article class="contenservuno">
+<section class="sesion2">
+  <h1 class="text-center" style="color: #fff;">SERVICIOS</h1>
+</section>
 
-        <div class="container">
-    <div class="row">
+<section class="sesion3">
+  <p class="text-muted text-center texto2">El transporte de carga requiere de experiencia en el mercado, esta experiencia la tiene INPET transportando carga en equipo especial. INPET se especializa en transportar carga con seguridad a su destino, respetando las normas internacionales y regulaciones locales. <br> <br>
 
-    <h1 class="text-centernosotros txt-propiedades" style="width: 100%;  margin-top: 5%; color: #3B3B3A; border-bottom: 1px black solid; padding-bottom: 1%;">Servicios <small>Consultoría en Administración</small></h1> <br><br><br>
+    Cada contenedor es supervisado antes de aceptar la carga a bordo. Con el fin de permitirnos ofrecerle un mejor servicio necesitamos recibir de usted toda la información requerida y la documentación pertinente.
+</p>
+</section>
 
-    <div class="col-sm-4">
-           <div class="panel panel-blue">
-            <div class="panel-heading">Desarrollo Organizacional</div>
-            <div class="panel-body">
-            <i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Diseño y Revisión de la Filosofía de la Empresa. <br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Estudios sobre Factores Clave que inciden en el &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;desempeño del personal (Clima, Satisfacción, &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rotación, etc.).<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Plan de Vida y Carrera.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Tabulador de Sueldos y Salarios.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Reingeniería.
-			</div>
+<section class="sesion2">
+  <div class="uno">
+    <h3 class="text-center">Fletamento de Buques</h3> <br>
+    <p>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Buques multipropósitos <br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Servicios de representación <br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Contratos de fletamento de buques
+y actividades de post-cierre <br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Administración y operación de buques <br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Corretaje de cargas (Dry cargo brokers)
+agentes fletadores y corretaje competitivo
+para cargas secas <br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Servicios de consultoría y asesoría para
+desarrollo de proyectos (Consulting services) <br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Investigación de mercados y estudios de factibilidad <br>
+    </p>
+  </div>
+  <div class="uno">
+     <h3 class="text-center">Transporte</h3> <br>
+    <p>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Supervisión de carga y descarga de mercancías generales y carga a granel <br> <br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Inspección de carga general convencional y contenerizada <br><br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Transportación marítima de carga en contenedores cargas convencionales y carga rodante.
+      
+    </p>
+  </div>
+  <div class="uno">
+    <h3 class="text-center">Agenciamiento y operaciones</h3> <br>
+    <p>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Representación en calidad de agentes protectores <br> 
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Mantenimiento y reparación de los contenedores <br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Control de contenedores.<br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Agenciamiento portuario <br> 
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Agenciamiento comercial <br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Atención a tripulantes<br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Manejo de refacciones<br>
+      <i class="fa fa-chevron-right" aria-hidden="true"></i> Suministro de combustible
+      
+    </p>
+  </div>
+</section>
+
+ <div class="section sesion3">
+      <div class="container">
+      <h1 class="text-center">Otros servicios</h1> <br><br> 
+        <div class="row">
+          <div class="col-md-4 col-md-offset-2">
+            <figure class="imgservicios">
+            <!-- <div class="fondo">
+              <div class="text-imgservicios">BODAS</div>
+            </div> -->
+          </figure>
+            <h3 class="text-center text-muted">Reparación y mantenimiento de otros artículos para el hogar</h3>
+            <p class="text-center text-muted">Conformado por ingenieros altamente capacitados en la reparación de refrigeradores, lavadoras, A/C, etc. Nuestros técnicos le brindarán confianza y seguridad al realizar su trabajo, y podrán resolver sus necesidades y sus consultas cuando lo necesite.</p>
           </div>
-        </div>
-        <div class="col-sm-4">
-           <div class="panel panel-pink">
-            <div class="panel-heading">Proceso Administrativo</div>
-            <div class="panel-body">
-            <i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Planeación (Objetivos, Estrategias, Programas).<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Organización (Descripción de Puestos Claves, &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Procesos y Procedimientos).<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Integración (Presupuestos, Recursos).<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Dirección (Mecanismos de Seguimiento).<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Control (Balance Score Card).
-			</div>
+          <div class="col-md-4">
+            <figure class="imgservicios2">
+            <!-- <div class="fondo">
+              <div class="text-imgservicios">BODAS</div>
+            </div> -->
+          </figure>
+            <h2 class="text-center text-muted">Servicios de instalaciones</h2>
+            <p class="text-center text-muted">Ingenieros, dibujantes, electricistas, carpinteros, modelistas y fontaneros, trabajan bajo un mismo objetivo: hacer excelentes todos los servicios.</p>
           </div>
+          
         </div>
-
-        <div class="col-sm-4">
-           <div class="panel panel-yellow">
-            <div class="panel-heading">Diagnóstico y Auditorías</div>
-            <div class="panel-body">
-            	<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Operativas.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Administrativas.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Aplicación de Indicadores del Desempeño.
-            </div>
-          </div>
-        </div>
-
-
+      </div>
+      <p class="boton">
+        <a href="contacto.php"><button type="button" class="btn btn-default">CONTACTO</button></a>
+     </p>
     </div>
-
-    <div class="row">
-        <div class="col-sm-4">
-           <div class="panel panel-pink">
-            <div class="panel-heading">Capacitación Focalizada</div>
-            <div class="panel-body">
-            Adecuada a Necesidades, por Actualización en Puestos y/o Requerimientos Solicitados a la Empresa.
-
-			Estamos comprometidos con sus resultados.
-			Por ello desarrollamos y aplicamos las técnicas más efectivas del mercado en materia de cursos de capacitación y desarrollo de competencias.
-			</div>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-           <div class="panel panel-yellow">
-            <div class="panel-heading">Administración Financiera</div>
-            <div class="panel-body">
-            <i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Definición de las estrategias de compra, venta, &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;fusión, escisión, etc.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Determinación de rangos de valor.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Determinación precios de compra - venta.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Definición de estrategias de negociación.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Actuación como intermediarios entre las partes &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;involucradas.
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-           <div class="panel panel-blue">
-            <div class="panel-heading">Cursos de Capacitación</div>
-            <div class="panel-body">
-            En NOKIN ofrecemos; certificaciones en administración y desarrollo organizacional.
-			</div>
-          </div>
-        </div>
-    </div>
-</div>
-
-</article>
-
-<article class="contenservdos">
-
-        <div class="container">
-    <div class="row">
-
-    <h1 class="text-centernosotros txt-propiedades" style="width: 100%;  margin-top: 5%; color: #3B3B3A; border-bottom: 1px black solid; padding-bottom: 1%;">Servicios <small>Consultoría Informática</small></h1> <br><br><br>
-        <div class="col-sm-4">
-           <div class="panel panel-pink">
-            <div class="panel-heading">Networking y Comunicaciones</div>
-            <div class="panel-body">
-            <i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Colaboración. <br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Redes LAN, WAN.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Comunicaciones unificadas.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Videoconferencias y Tele presencia.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Redes empresariales(SW y ROUTER).<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Centro de Datos y Virtualización.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Soluciones de almacenamiento.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Seguridad informática.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Redes Inalámbricas.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Equipos ópticos.
-			</div>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-           <div class="panel panel-yellow">
-            <div class="panel-heading">Infraestructura y Conectividad</div>
-            <div class="panel-body">
-            	<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Infraestructura en Energización de equipos de &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Telecomunicaciones.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Infraestructura de Cableado estructurado ( Voz, &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Datos y Video).<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Infraestructura en Fibra óptica(Planta Externa/ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Interna,).<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Infraestructura en Adecuación de Cuarto de &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Telecomunicaciones(site).<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Infraestructura en CCTV.
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-4">
-           <div class="panel panel-blue">
-            <div class="panel-heading">Estrategia y Planeación Tecnológica</div>
-            <div class="panel-body">
-            <i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Planteamiento de Escenarios Tecnológicos y de &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Inversión. <br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Asesoramiento sobre tecnologías.<br>
-			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Entendimiento de las necesidades del Negocio.<br>
-			</div>
-          </div>
-        </div>
-
-    </div>
-
-    <div class="row">
-        <div class="col-sm-4">
-           <div class="panel panel-pink">
-            <div class="panel-heading">Consultoría en Servicios</div>
-            <div class="panel-body">
-            Servicios de Consultoría Especializados en la mejora de los procesos organizacionales y de TIC bajo el respaldo profesional y del personal altamente calificado, certificado y con habilidades y conocimientos necesarios para cumplir el objetivo y la misión empresarial y/o institucional.
-			</div>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-           <div class="panel panel-yellow">
-            <div class="panel-heading">Fabrica de Software y Seguridad Aplicativa</div>
-            <div class="panel-body">
-            	Área de especialización de desarrollo de soluciones de sistemas de información con respaldo profesional y del personal altamente calificado, certificado, con conocimientos y experiencia necesarios para llevar acabo el objetivo y la misión empresarial y/o institucional.
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-           <div class="panel panel-blue">
-            <div class="panel-heading">Cursos de Capacitación</div>
-            <div class="panel-body">
-            En NOKIN ofrecemos; certificaciones en seguridad y soporte.
-			</div>
-          </div>
-        </div>
-    </div>
-</div>
-<br><br>
-        <div style="text-align: center; width: 100%;">
-  <a href="contacto.php"><button type="button" class="btn btn-danger btn-lg">Contáctanos</button></a>
-</div>
-<br><br><br>
-</article>
-
 
 <span class="ir-arriba"><i class="fa fa-chevron-up" aria-hidden="true"></i></span>
 <?php include 'pie.php' ?>
